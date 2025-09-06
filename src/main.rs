48a77f2f423a3a547447e9c120fd929982921a5f
Aurora Long <aurora.long@tuta.io>
2025-09-06 18:11:49 +1000

    Fix: issue #2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::process::Stdio;
 use clap::{Parser, ValueEnum};
 use fantoccini::{ClientBuilder, Locator};
 use serde_json::json;
+use std::process::Stdio;
 use tokio::process;
 
 /// A program to automate claiming itch.io games purchased as part of large bundles
@@ -17,11 +17,11 @@ struct Args {
     password: String,
 
     /// bundle name
-    #[arg(short, long, required=true)]
+    #[arg(short, long, required = true)]
     bundle: Vec<String>,
 
     /// select webdriver to use (should be installed on your system and in PATH)
-    #[arg(short, long, default_value="chrome")]
+    #[arg(short, long, default_value = "chrome")]
     webdriver: WebDriver,
 }
 
@@ -29,9 +29,9 @@ struct Args {
 enum WebDriver {
     /// use chromedriver
     Chrome,
-    
+
     /// use geckodriver
-    Firefox
+    Firefox,
 }
 
 #[tokio::main]
@@ -44,53 +44,92 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         WebDriver::Chrome => "chromedriver",
         WebDriver::Firefox => "geckodriver",
     })
-        .arg("--port=4444").kill_on_drop(true).stdout(Stdio::null())
-        .spawn().expect("Failed to start WebDriver, perhaps it isn't installed?");
+    .arg("--port=4444")
+    .kill_on_drop(true)
+    .stdout(Stdio::null())
+    .spawn()
+    .expect("Failed to start WebDriver, perhaps it isn't installed?");
 
     // start headless client and connect to webdriver
     let client = ClientBuilder::native()
         .capabilities(serde_json::Map::from_iter([
-            ("moz:firefoxOptions".into(), json!({
-                "args": ["-headless"]
-            })),
-            ("goog:chromeOptions".into(), json!({
-                "args": ["--headless"]
-            })),
+            (
+                "moz:firefoxOptions".into(),
+                json!({
+                    "args": ["-headless", "-private"]
+                }),
+            ),
+            (
+                "goog:chromeOptions".into(),
+                json!({
+                    "args": ["--headless", "--incognito"]
+                }),
+            ),
         ]))
-        .connect("http://localhost:4444").await?;
+        .connect("http://localhost:4444")
+        .await?;
 
-    
     // login to itch.io using supplied credentials
     client.goto("https://itch.io/login").await?;
-    client.form(Locator::Css(".login_form_widget > form")).await?
-        .set_by_name("username", &args.username).await?
-        .set_by_name("password", &args.password).await?
-        .submit_direct().await?;
+    client
+        .form(Locator::Css(".login_form_widget > form"))
+        .await?
+        .set_by_name("username", &args.username)
+        .await?
+        .set_by_name("password", &args.password)
+        .await?
+        .submit_direct()
+        .await?;
     println!("Logging in to itch.io as {:?}", &args.username);
-    
-    // wait for login to complete
-    client.wait().for_element(Locator::Css("[data-page_name='dashboard']")).await?;
 
+    // wait for login to complete
+    // client.wait().for_element(
+    //     Locator::Css("[data-page_name='dashboard'],\
+    //         [data-page_name='my_feed']")).await?;
+    client
+        .wait()
+        .for_element(Locator::Css(
+            "[data-page_name]:not([data-page_name='user_login'])",
+        ))
+        .await?;
 
     for bundle in &args.bundle {
         client.goto("https://itch.io/my-purchases/bundles").await?;
         println!("Claiming bundle: {:?}", &bundle);
-        client.find(Locator::LinkText(&bundle)).await?.click().await?;
+        client
+            .find(Locator::LinkText(&bundle))
+            .await?
+            .click()
+            .await?;
 
         loop {
             // log current page number
-            println!("{}", client.find(Locator::Css(".pager_label")).await?.text().await?);
-            
+            println!(
+                "Page {}",
+                client
+                    .find(Locator::Css(".pager_label"))
+                    .await?
+                    .text()
+                    .await?
+            );
+
             // claim first unclaimed game on page
             while let Ok(game) = client.find(Locator::Css("button[value=claim]")).await {
                 game.click().await?;
-                
-                // log the game's name
-                println!("{:?} claimed",
-                    client
-                        .find(Locator::Css(".game_download_header_widget .object_title")).await?
-                        .text().await?);
-            
+
+                match client
+                    .find(Locator::Css(".game_download_header_widget .object_title"))
+                    .await
+                {
+                    Ok(title) => {
+                        // log the game's name
+                        println!("{:?} claimed", title.text().await?);
+                    }
+                    Err(_) => {
+                        let url = client.current_url().await?;
+                        eprintln!("something went wrong while claiming {:?}", url.to_string())
+                    }
+                }
                 client.back().await?;
             }
 
@@ -101,7 +140,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 break;
             }
         }
-        
     }
 
     // clean up
@@ -109,5 +147,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     driver.kill().await?;
 
     Ok(())
-
 }
